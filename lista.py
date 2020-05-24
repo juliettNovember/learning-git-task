5ea47d61c029c37972425f62ac8510b815bc7b52
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-05-24 11:47:42 +0200

    Add value

--- lista.py
+++ lista.py
@@ -1,7 +1,8 @@
 text = "Lista zakupów" '\n'
 lista_zakupów = {
     "piekarnia": ["chleb", "bułki", "pączek"],
-    "warzywniak": ["marchew", "seler", "rukola"]
+    "warzywniak": ["marchew", "seler", "rukola"],
+    "cukiernia": ["rogalik", "beza"]
 }
 n=0
 
@@ -9,6 +10,6 @@ for key, values in lista_zakupów.items():
     text += f"Idę do {key.title()}, kupuję tam: {values}. \n"
     n = n + len(values)
 
-text += f"W sumię kupuję {n} produktów."
+text += f"W sumię kupuję {n} produktów, gdyz wstąpiłam do cukierni"
 
 print(text)
