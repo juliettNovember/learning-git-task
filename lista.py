a365f8c34f5872c96625f7ad0f97c1ebd1208053
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-05-24 12:00:15 +0200

    Add regards

--- lista.py
+++ lista.py
@@ -2,7 +2,7 @@ text = "Lista zakupów" '\n'
 lista_zakupów = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"],
-    "cukiernia": ["rogalik", "beza"]
+    "cukiernia": ["rogalik", "beza", "sernik"]
 }
 n=0
 
@@ -10,6 +10,8 @@ for key, values in lista_zakupów.items():
     text += f"Idę do {key.title()}, kupuję tam: {values}. \n"
     n = n + len(values)
 
-text += f"W sumię kupuję {n} produktów, gdyz wstąpiłam do cukierni"
+text += f"W sumię kupuję {n} produktów, gdyz wstąpiłam do cukierni. \n"
 
+
+text += "Cięzko jest to ogarnąć, ale walczę dzielnie! Pozdrowienia :)"
 print(text)
